45a861f4e924992e1fb8b823c80775153ef8e305
SpringAutumn09 <springautumnmoon@proton.me>
2025-11-19 15:54:30 +0330

    Separated file for the unit convertor

---
+++ WeatherDashboard/UnitConverter.py
@@ -0,0 +1,12 @@
+def KelvinToCelsius(k):
+    c = k - 273
+
+def CelsiusToFahrenheit(c):
+    f = (c)*9/5 + 32
+
+def FahrenheitToCelsius(f):
+    c = (f)*5/9 -32
+
+def hPaToInHg(hpa):
+    inhg = hpa * 0.02953
+
