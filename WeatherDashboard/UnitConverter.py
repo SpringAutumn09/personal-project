49c02e591c09da3e962639ace3fafdf65e2f4502
SpringAutumn09 <springautumnmoon@proton.me>
2025-11-19 16:15:40 +0330

    Added rounding function

--- WeatherDashboard/UnitConverter.py
+++ WeatherDashboard/UnitConverter.py
@@ -10,3 +10,13 @@ def FahrenheitToCelsius(f):
 def hPaToInHg(hpa):
     inhg = hpa * 0.02953
 
+def round_float(num, decimals=0):
+    # Shift the decimal point to the right
+    multiplier = 10 ** decimals
+    
+    # Multiply the number by the multiplier, round it to the nearest integer, and then shift the decimal back
+    rounded = int(num * multiplier + 0.5) if num > 0 else int(num * multiplier - 0.5)
+    
+    # Divide back to get the rounded value
+    return rounded / multiplier
+
